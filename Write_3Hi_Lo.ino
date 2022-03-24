1f6aef8f56c08c85ed9c761e59d22140c86be50e
kcc40 <59624419+kcc40@users.noreply.github.com>
2022-03-24 10:57:48 -0400

    Update Write_3Hi_Lo.ino
    Instead of off, when the button is not pressed the LEDs will be lit. Also, changed formatting in if statement.

--- Write_3Hi_Lo.ino
+++ Write_3Hi_Lo.ino
@@ -11,22 +11,22 @@ void setup() {
 void loop() {
   if(digitalRead(9)==HIGH)  //checks for button press
   {
-  // put your main code here, to run repeatedly:
-  digitalWrite(10,HIGH);  //sets the digital pin 10 high
-  delay(1000);            //waits 1s
-  digitalWrite(10,LOW);   //sets digital pin 10 low
-  digitalWrite(11,HIGH);  //sets the digital pin 11 high
-  delay(1000);            //waits 1s
-  digitalWrite(11,LOW);   //sets digital pin 11 low
-  digitalWrite(12,HIGH);  //sets the digital pin 12 on
-  delay(1000);            //waits 1s
-  digitalWrite(12,LOW);   //sets digital pin 12 low
+    // put your main code here, to run repeatedly:
+    digitalWrite(10,HIGH);  //sets the digital pin 10 high
+    delay(1000);            //waits 1s
+    digitalWrite(10,LOW);   //sets digital pin 10 low
+    digitalWrite(11,HIGH);  //sets the digital pin 11 high
+    delay(1000);            //waits 1s
+    digitalWrite(11,LOW);   //sets digital pin 11 low
+    digitalWrite(12,HIGH);  //sets the digital pin 12 on
+    delay(1000);            //waits 1s
+    digitalWrite(12,LOW);   //sets digital pin 12 low
   }
   else
   {
-    digitalWrite(10,LOW);   //sets digital pin 10 low
-    digitalWrite(11,LOW);   //sets digital pin 11 low
-    digitalWrite(12,LOW);   //sets digital pin 12 low
+    digitalWrite(10,HIGH);   //sets digital pin 10 low
+    digitalWrite(11,HIGH);   //sets digital pin 11 low
+    digitalWrite(12,HIGH);   //sets digital pin 12 low
   }
   
 }
